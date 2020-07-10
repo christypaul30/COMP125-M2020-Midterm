947d4f20783995fe5c77315752b9095af52dba63
Christy Paul <christypaul59@gmail.com>
2020-07-10 17:31:21 -0400

    more js mods

--- COMP125-M2020-MidTerm/Scripts/app.js
+++ COMP125-M2020-MidTerm/Scripts/app.js
@@ -2,11 +2,62 @@
 (function () {
 "use strict";
 
+function Start()
+{
+
+    console.log('%App started...', "color:white; font-size: 24px;");
+
+let para = document.createElement("P");                       // Create a <p> node
+let intro = document.createTextNode("This is intro paragraph.")[0];      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("IntroParagraph").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let folegandros = document.createTextNode("This is second paragraph.")[1];      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Folegandros").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is third paragraph.")[2];      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Alonissos").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is a paragraph.")[3];      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Spetses").appendChild(para);           // Append <p> to <div> with id="container"
 
 let para = document.createElement("P");                       // Create a <p> node
 let t = document.createTextNode("This is a paragraph.");      // Create a text node
 para.appendChild(t);                                          // Append the text to <p>
-document.getElementById("myDIV").appendChild(para);           // Append <p> to <div> with id="myDIV"
+document.getElementById("Amorgos").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is a paragraph.");      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Syros").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is a paragraph.");      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Milos").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is a paragraph.");      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Hydra").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is a paragraph.");      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Ithaca").appendChild(para);           // Append <p> to <div> with id="container"
+
+let para = document.createElement("P");                       // Create a <p> node
+let t = document.createTextNode("This is a paragraph.");      // Create a text node
+para.appendChild(t);                                          // Append the text to <p>
+document.getElementById("Gavdos").appendChild(para)[9];           // Append <p> to <div> with id="container"
+
+}
 
 
 
